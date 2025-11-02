46aaa3bcce56c0e34c30164db768a42952b0ef98
Mech-Kal <kalpakkamble02@gmail.com>
2025-11-02 21:30:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,8 +14,3 @@ function secondHighest(arr) {
 }
 
 // Example Test Cases
-console.log(secondHighest([5, 1, 2, 3, 4]));  // 4
-console.log(secondHighest([-1, -2, -3, -4, -5]));  // -2
-console.log(secondHighest([]));  // -Infinity
-console.log(secondHighest([1]));  // -Infinity
-console.log(secondHighest([1, 1, 1, 1, 1]));  // -Infinity

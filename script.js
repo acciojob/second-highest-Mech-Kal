d43db81ba8d615a43017748d5354f82832df3a75
Mech-Kal <kalpakkamble02@gmail.com>
2025-11-18 01:48:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 function secondHighest(arr) {
+  // Convert all input values to numbers
+  arr = arr.map(Number);
+
   // If array has less than 2 elements, return -Infinity
   if (arr.length < 2) return -Infinity;
 
   // Remove duplicates
   let uniqueArr = [...new Set(arr)];
 
-  // If all elements are same, return -Infinity
+  // If all elements are the same, return -Infinity
   if (uniqueArr.length < 2) return -Infinity;
 
-  // Sort in descending order and return the second element
+  // Sort numbers in descending order
   uniqueArr.sort((a, b) => b - a);
+
+  // Return the second highest number
   return uniqueArr[1];
 }
 
-// Example Test Cases
+function Main() {
+  var n = prompt("Enter the number of elements");
+  var arr = [];
+
+  for (var i = 0; i < n; i++) {
+    arr[i] = prompt("Enter element " + (i + 1));
+  }
+
+  alert(secondHighest(arr));
+}
+
+Main();
